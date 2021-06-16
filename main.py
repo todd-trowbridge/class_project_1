d4b30b8dec6155c6707145fa20011998f66c9f96
Todd Trowbridge <todd.trowbridge@icloud.com>
2021-06-16 09:46:26 -0400

    formatting

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import praw
 
 # IMPORTANT:
 # the first time running you'll need to create a setup.json file in the data directory
-# copy the next line of code (line 7) inside data.json without the # 'pound' sign
+# copy the next line of code (line 7) inside data.json and remove the comment sign '#'
 # {"setup": [{"client_id": "", "client_secret": "", "password": "", "user_agent": "testscript by u/", "username": ""}]}
 # fill in each empty "" with info from your reddit account, see https://redditclient.readthedocs.io/en/latest/oauth/ for more info
 
 # setup
+
 print('\n running setup')
 bot = Bot()
 print('setup finished \n')
 
 # run until something breaks
 # ctrl + c stops the app in terminal
+
 while True:
   # setup subreddit stream of comments
   for comment in bot.r.subreddit(bot.subreddits).stream.comments():
